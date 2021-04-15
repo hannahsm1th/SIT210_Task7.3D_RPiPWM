853a56414236233f62236eba10e71e146d1d2c16
hannah-smith0 <48146699+hannah-smith0@users.noreply.github.com>
2021-04-15 18:52:51 +1000

    Update ultrasonic_distance.py

--- ultrasonic_distance.py
+++ ultrasonic_distance.py
@@ -1,7 +1,7 @@
 #Libraries
 import RPi.GPIO as GPIO
 import time
-from gpiozero import PWMLED
+from gpiozero import PWMLED #This is used to control the LED brightness
 
 #GPIO Mode
 GPIO.setmode(GPIO.BCM)
@@ -11,19 +11,20 @@ TRIGGER = 23
 ECHO = 24
 LED = PWMLED(17)
 
-#GPIO direction
+#GPIO direction for proximity sensor
 GPIO.setup(TRIGGER, GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
 def distance():
 
-    GPIO.output(TRIGGER, False)
+    GPIO.output(TRIGGER, False) #This stops the sensor from firing too often
     time.sleep(1)
 
     GPIO.output(TRIGGER, True)
     time.sleep(0.00001)
     GPIO.output(TRIGGER, False)
-
+    
+    #This measures the time it takes to receive the information
     while GPIO.input(ECHO)==0:
         pulse_start = time.time()
     while GPIO.input(ECHO)==1:
@@ -31,18 +32,20 @@ def distance():
     
     duration = pulse_end - pulse_start
     distance = duration * 17150
-    distance = round(distance, 2)
+    distance = round(distance, 2
+
     return distance
 
-MAX_DISTANCE = 30 #Longest distance in the room
+MAX_DISTANCE = 30 #This is how far you want the sensor data to be monitored by the LED
+                  #I chose 30cm so that I could easily demo my project in the video
 
 try:
     while True:
         currentDistance = distance()
-        if currentDistance > 30:
+        if currentDistance > MAX_DISTANCE:
             LED.value = 0
-        if currentDistance <= 30:
-            proximity = 1-(currentDistance / MAX_DISTANCE)
+        if currentDistance <= MAX_DISTANCE:
+            proximity = 1-(currentDistance / MAX_DISTANCE) #The PWM value needs to be reversed so the LED gets brighter as you get closer
             LED.value = proximity
         time.sleep(.01)
 except KeyboardInterrupt:
